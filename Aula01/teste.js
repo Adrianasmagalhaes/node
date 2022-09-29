1511c0a5b872eba9fea08edec052efa67ae1e403
Adriana sm <a.s.m27267@gmail.com>
2022-09-29 19:47:22 -0300

    inserindo contador setInterval

--- Aula01/teste.js
+++ Aula01/teste.js
@@ -1,15 +1,20 @@
-//CommonJS -> responsável por importar ou exportar os módulos 
+//CommonJS -> responsável por importar ou exportar os módulos
 // Importando o módulo 'os' do node e colocando em uma variável
-const os = require ('os')
+const os = require("os");
 // o módulo 'os ' está dentro da variável 'os', desta forma conseguimos acessar as propriedades e métodos deste módulo/objeto
-const totalMen= parseInt(os.totalmem()/1024 /1024)
-const freMen= parseInt(os.freemem()/1024 /1024)
 
-const memoria ={
-    total:  `${totalMen}MB`,
-    free:   ` ${freMen}MB`
-}
+setInterval(() => {
+  const totalMen = parseInt(os.totalmem() / 1024 / 1024);
+  const freMen = parseInt(os.freemem() / 1024 / 1024);
+  const percent = parseInt((freMen/totalMen )*100)
 
-console.table(memoria)
+  const memoria = {
+    total: `${totalMen}MB`,
+    free: ` ${freMen}MB`,
+    percent: `${percent}%`,
+  }
+  console.clear();
+  console.table(memoria);
+}, 1000);
 
 // console.log(`Memória Livre: ${freMen}`, `Total de Memória : ${totalMen}`);
